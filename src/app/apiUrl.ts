ba2767d4984ee0776b7fad620965a7a261bff509
Paramananda <paramananda.biswal@spx.com>
2019-09-17 15:50:00 +0530

    Build 1

--- src/app/apiUrl.ts
+++ src/app/apiUrl.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
       // public static URL = 'http://api.wisereads.in';
-      public static URL = 'http://af9363f77d92a11e9bede12b66f34a40-2101243239.us-east-1.elb.amazonaws.com:8080';
+      public static URL = 'http://a2327f647d93411e9bede12b66f34a40-1894799224.us-east-1.elb.amazonaws.com:8080';
 }
